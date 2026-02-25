561d415e576539f431cfedf80002bbc4bdb38876
Ryjen1 <your-github-email@example.com>
2026-02-25 14:48:06 +0100

    chore: fix build and jest for ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).ts']
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        isolatedModules: true,
+        diagnostics: {
+          ignoreCodes: [151002],
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
--- src/openapi.ts
+++ src/openapi.ts
@@ -108,4 +108,3 @@ export const openApiSpec = {
     },
   },
 } as const;
-
