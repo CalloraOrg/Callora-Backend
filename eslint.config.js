c4c1f300f97915ffdf4f5f1f27d60578c879998b
moreluck <luckykamshak@gmail.com>
2026-02-25 14:33:45 +0100

    chore: stabilize lint and test pipeline

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+    }
+  }
+];
