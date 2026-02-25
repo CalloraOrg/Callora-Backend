c40369ebfcd0bd95104c8c681383c519e1f73143
Your Name <you@example.com>
2026-02-25 15:02:47 +0100

    chore: fix jest config for ESM project

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.json' }]
+  }
+};
