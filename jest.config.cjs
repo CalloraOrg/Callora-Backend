cb287e3a015d43e307ea1062f42fab2c9f778181
Anioke Sebastian <aniokechukwudi7@gmail.com>
2026-02-26 11:15:00 +0100

    test: separate jest specs from node test runner

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	testMatch: ["**/?(*.)+(spec|test).ts"],
+	testMatch: ["**/*.spec.ts"],
 	moduleNameMapper: {
 		"^(\\.{1,2}/.*)\\.js$": "$1",
 	},
